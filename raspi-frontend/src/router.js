74648a63b5140f11fb22e30cb0a05ec3dda6ecef
Jeremie Beaulieu <jeremiebeaulieu222@gmail.com>
2019-02-08 16:14:22 -0500

    reports page completed with hardcoded data (rebased)

--- raspi-frontend/src/router.js
+++ raspi-frontend/src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import Stats from './views/Stats.vue';
 import SignUp from './views/SignUp.vue';
 
 Vue.use(Router);
@@ -10,6 +11,10 @@ export default new Router({
     path: "/",
     name: "home",
     component: Home
+  }, {
+    path: "/stats",
+    name: "stats",
+    component: Stats
   }, {
     path: "/sign-up",
     name: "sign-up",
