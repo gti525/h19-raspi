ad56a2100f1c63e519158b10211eb21b7e85e9fc
Gabriel Périard-Tremblay <gabriel@periard.ca>
2019-03-15 11:17:57 -0400

    Ticket validator base

---
+++ ticket_validator/ticket_validator/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.utils.translation import gettext as _
+
+
+class ScanLog(models.Model):
+    SCAN_STATUS = (
+        ('OK', _('Scanné, OK')),
+        ('FAIL', _('Déjà scanné! FAIL')),
+    )
+
+    created = models.DateTimeField(auto_now_add=True)
+    ticket_uuid = models.UUIDField(editable=False)
+    status = models.CharField(choices=SCAN_STATUS, max_length=4)
+
+    def __str__(self):
+        return self.ticket_uuid
+
+
+class Ticket(models.Model):
+    uuid = models.UUIDField(editable=False, unique=True)
+    show = models.UUIDField(editable=False)
+    scanned = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.ticket_uuid
+
