001b3bbde0adaba29d49bff58be8b2d8ccd85ef3
Gabriel Périard-Tremblay <gabriel@periard.ca>
2019-03-26 13:23:42 -0400

    static changes

--- venue/venue/settings.py
+++ venue/venue/settings.py
@@ -145,9 +145,7 @@ STATIC_URL = '/'
 # STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 # Place static in the same location as webpack build files
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'raspi-frontend', 'dist')
-]
+STATICFILES_DIRS = []
 
 # DJANGO REST FRAMEWORK
 REST_FRAMEWORK = {
